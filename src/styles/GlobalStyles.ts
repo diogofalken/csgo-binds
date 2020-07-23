5d3ff40ea3d9e0afa388d06fc4252d10c2d2d682
diogofalken <diogofalken0@gmail.com>
2020-07-23 09:12:17 +0100

    :lipstick: Created global css styles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,38 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+    color: var(--white);
+  }
+
+  html, body, #root {
+    max-height: 100vh;
+    max-width: 100vw;
+
+    width: 100%;
+    height: 100%;
+  }
+
+  *, button, input {
+    border: 0;
+    background: none;
+    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  html {
+    background: var(--primary);
+  }
+
+  :root {
+    --primary: #E5E5E5;
+    --secondary: #00AFB5;
+    --black: #202327;
+    --white: #FFF;
+    --gray: #7A7A7A;
+    --pink: EC0868;
+  }
+`;
