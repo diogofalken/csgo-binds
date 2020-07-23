8529fc297d86cf4ae9d7a9e9f04d210917d4b323
diogofalken <diogofalken0@gmail.com>
2020-07-23 09:18:22 +0100

    :sparkles: Created Layout structure

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Layout: React.FC = () => {
+  return (
+    <Container>
+      {/* <Header></Header> */}
+
+      {/* <Main></Main> */}
+
+      {/* <Footer></Footer> */}
+    </Container>
+  );
+};
+
+export default Layout;
